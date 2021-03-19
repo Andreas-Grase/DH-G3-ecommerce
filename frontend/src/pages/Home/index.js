752b5d63d2a780f1ae842a32c1dedc996bdbdaab
Andreas Grase <andreas.grase@gmail.com>
2021-03-18 22:26:08 -0300

    React - estrutura preparada

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,23 +1,10 @@
-import logo from "../../assets/img/logo.svg";
-
 import "./styles.css";
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="">
+       <h1>Hello World</h1>
       </header>
     </div>
   );
