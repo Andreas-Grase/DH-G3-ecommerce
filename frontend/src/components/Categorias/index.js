4b2a2bf241049883259e7e7e14dd4912eaf49831
alineogassawara <linepsouza@gmail.com>
2021-03-22 23:36:17 -0300

    trocando imagens de categorias

--- frontend/src/components/Categorias/index.js
+++ frontend/src/components/Categorias/index.js
@@ -1,8 +1,8 @@
-import imagemteste1 from '../../assets/img/nossos-produtos/imagemteste1.jpg'
-import imagemteste2 from '../../assets/img/nossos-produtos/imagemteste2.jpg'
-import imagemteste3 from '../../assets/img/nossos-produtos/imagemteste3.jpg'
-import imagemteste4 from '../../assets/img/nossos-produtos/imagemteste4.jpg'
-import imagemteste5 from '../../assets/img/nossos-produtos/imagemteste5.jpg'
+import barbearia from '../../assets/img/nossos-produtos/barbearia.svg'
+import cabelo from '../../assets/img/nossos-produtos/cabelo.svg'
+import lancamentos from '../../assets/img/nossos-produtos/lancamentos.svg'
+import pele from '../../assets/img/nossos-produtos/pele.svg'
+import unhas from '../../assets/img/nossos-produtos/unhas.svg'
 import './style.css'
 
 const Categorias = () => {
@@ -13,11 +13,11 @@ const Categorias = () => {
                 <h4>Conheça nossas categorias</h4>
             </div>
         <div id="ladoalado">
-          <a href="shampoo.html"><img class="img-produtos1" src={imagemteste1} alt="shampoo" /></a>
-          <a href="condicionador.html"><img class="img-produtos" src={imagemteste2} alt="condicionador" /></a>
-          <a href="mascara.html"><img class="img-produtos1" src={imagemteste3} alt="mascaras" /></a>
-          <a href="cremes"><img class="img-produtos" src={imagemteste4} alt="cremes" /></a>
-          <a href="barbearia"><img class="img-produtos1" src={imagemteste5} alt="barbearia" /></a>
+          <a href="shampoo.html"><img class="img-produtos1" src={barbearia} alt="shampoo" /></a>
+          <a href="condicionador.html"><img class="img-produtos" src={cabelo} alt="condicionador" /></a>
+          <a href="mascara.html"><img class="img-produtos1" src={lancamentos} alt="mascaras" /></a>
+          <a href="cremes"><img class="img-produtos" src={pele} alt="cremes" /></a>
+          <a href="barbearia"><img class="img-produtos1" src={unhas} alt="barbearia" /></a>
         </div>
       </section>
     )
