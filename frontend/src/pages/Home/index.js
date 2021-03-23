3fce4a2c6d3a96eafb3acac90a9881804dadc00e
tatasevilla <tatasevilla2@hotmail.com>
2021-03-23 03:27:33 -0300

    footer react finalizado

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,13 +1,13 @@
+import Footer from "../../components/Footer";
 import Marcas from "../../components/Marcas";
 import "./styles.css";
 
 const Home = () => {
   return (
-    <div className="">
-      <marcas className="">
-          <Marcas />
-      </marcas>
-    </div>
+    <>
+          <Marcas />          
+          <Footer />
+    </>
   );
 }
 
