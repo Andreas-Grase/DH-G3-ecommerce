1b47eccf79fead3c1e60beab49d327160658d0b0
Eduardo-Azuolas <eduardoazuolas@gmail.com>
2021-03-24 19:52:11 -0300

    feature: react navbar

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Marcas from "../../components/Marcas";
 import Categorias from "../../components/Categorias";
@@ -7,6 +8,7 @@ import "./styles.css";
 const Home = () => {
   return (
     <>
+      <Header />
       <SliderHome />
       <Categorias />
       <Marcas />
