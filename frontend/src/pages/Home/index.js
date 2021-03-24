825288a6e6f3ef3316786ed3444a02feb484ab49
Andreas Grase <andreas.grase@gmail.com>
2021-03-24 19:16:49 -0300

    feature:promo-react

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import Marcas from "../../components/Marcas";
 import Categorias from "../../components/Categorias"
 import "./styles.css";
+import Ofertas from "../../components/Ofertas";
 
 const Home = () => {
   return (
     <div className="">
           <Categorias />
+          <Ofertas />
           <Marcas />
     </div>
   );
