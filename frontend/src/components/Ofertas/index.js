eb86d45366efa09de2d8317c9b2ca28d2d3c5e58
Andreas Grase <andreas.grase@gmail.com>
2021-03-26 20:37:03 -0300

    Ofertas:state-implementation-final-part

--- frontend/src/components/Ofertas/index.js
+++ frontend/src/components/Ofertas/index.js
@@ -8,6 +8,60 @@ import { useState } from 'react'
 import "./style.css"
 import React from 'react'
 
+const SlideOfertas = () => {
+    return (
+        <div className="slide-ofertas">
+
+            <div className="produto-slide-promo">
+                <img class="slide-promo-img" src={produto1} alt="shampoo" />
+                <p>Marca: <strong>Head and Shoulders</strong></p>
+                <p>Produto: <strong>Cabelos lisos 3000</strong></p>
+                <p>Preço: <strong>R$ 499,99</strong></p>
+            </div>
+
+            <div class="produto-slide-promo">
+                <img class="slide-promo-img" src={produto2} alt="shampoo" />
+                <p>Marca: <strong>Head and Shoulders</strong></p>
+                <p>Produto: <strong>Cabelos lisos 3000</strong></p>
+                <p>Preço: <strong>R$ 499,99</strong></p>
+            </div>
+
+            <div class="produto-slide-promo">
+                <img class="slide-promo-img" src={produto3} alt="shampoo" />
+                <p>Marca: <strong>Head and Shoulders</strong></p>
+                <p>Produto: <strong>Cabelos lisos 3000</strong></p>
+                <p>Preço: <strong>R$ 499,99</strong></p>
+            </div>
+
+        </div>
+    )
+}
+
+const SlideMV = () => {
+    return (
+        <div className="slide-mais-vendidos">
+            <div className="produto-slide-promo">
+                <img className="slide-promo-img" src={produto4} alt="shampoo" />
+                <p>Marca: <strong>Head and Shoulders</strong></p>
+                <p>Produto: <strong>Cabelos lisos 3000</strong></p>
+                <p>Preço: <strong>R$ 499,99</strong></p>
+            </div>
+            <div className="produto-slide-promo">
+                <img className="slide-promo-img" src={produto5} alt="shampoo" />
+                <p>Marca: <strong>Head and Shoulders</strong></p>
+                <p>Produto: <strong>Cabelos lisos 3000</strong></p>
+                <p>Preço: <strong>R$ 499,99</strong></p>
+            </div>
+            <div className="produto-slide-promo">
+                <img className="slide-promo-img" src={produto6} alt="shampoo" />
+                <p>Marca: <strong>Head and Shoulders</strong></p>
+                <p>Produto: <strong>Cabelos lisos 3000</strong></p>
+                <p>Preço: <strong>R$ 499,99</strong></p>
+            </div>
+        </div>
+    )
+}
+
 const Ofertas = () => {
 
     // uso do state de acordo com o exemplo em https://github.com/SerraZ3/react-js-introducao
@@ -15,16 +69,14 @@ const Ofertas = () => {
     // declaração do state
     // a primeira variável contém o valor do state
     // a segunda variável contém a função que irá alterar esse valor
-    const [slide, setSlide] = useState(0)
+    const [slideAtual, setSlide] = useState(0)
 
     const setSlideMV = () => {
-        document.querySelector(".slide-ofertas").style.display="none";
-        document.querySelector(".slide-mais-vendidos").style.display="flex";
+        setSlide(SlideMV)
     }
 
     const setSlideOfertas = () => {
-        document.querySelector(".slide-mais-vendidos").style.display="none";
-        document.querySelector(".slide-ofertas").style.display="flex";
+        setSlide(SlideOfertas)
     }
 
     return (
@@ -42,55 +94,7 @@ const Ofertas = () => {
 
             {/* slideshow container */}
             <div className="slideshow-promo-container">
-                
-                {/* slide "ofertas" */}
-                <div className="slide-ofertas">
-
-                    <div className="produto-slide-promo">
-                        <img class="slide-promo-img" src={produto1} alt="shampoo" />
-                        <p>Marca: <strong>Head and Shoulders</strong></p>
-                        <p>Produto: <strong>Cabelos lisos 3000</strong></p>
-                        <p>Preço: <strong>R$ 499,99</strong></p>
-                    </div>
-
-                    <div class="produto-slide-promo">
-                        <img class="slide-promo-img" src={produto2} alt="shampoo" />
-                        <p>Marca: <strong>Head and Shoulders</strong></p>
-                        <p>Produto: <strong>Cabelos lisos 3000</strong></p>
-                        <p>Preço: <strong>R$ 499,99</strong></p>
-                    </div>
-
-                    <div class="produto-slide-promo">
-                        <img class="slide-promo-img" src={produto3} alt="shampoo" />
-                        <p>Marca: <strong>Head and Shoulders</strong></p>
-                        <p>Produto: <strong>Cabelos lisos 3000</strong></p>
-                        <p>Preço: <strong>R$ 499,99</strong></p>
-                    </div>
-
-                </div>
-
-                {/* slide "mais vendidos" */}
-                <div className="slide-mais-vendidos">
-                    <div className="produto-slide-promo">
-                        <img className="slide-promo-img" src={produto4} alt="shampoo" />
-                        <p>Marca: <strong>Head and Shoulders</strong></p>
-                        <p>Produto: <strong>Cabelos lisos 3000</strong></p>
-                        <p>Preço: <strong>R$ 499,99</strong></p>
-                    </div>
-                    <div className="produto-slide-promo">
-                        <img className="slide-promo-img" src={produto5} alt="shampoo" />
-                        <p>Marca: <strong>Head and Shoulders</strong></p>
-                        <p>Produto: <strong>Cabelos lisos 3000</strong></p>
-                        <p>Preço: <strong>R$ 499,99</strong></p>
-                    </div>
-                    <div className="produto-slide-promo">
-                        <img className="slide-promo-img" src={produto6} alt="shampoo" />
-                        <p>Marca: <strong>Head and Shoulders</strong></p>
-                        <p>Produto: <strong>Cabelos lisos 3000</strong></p>
-                        <p>Preço: <strong>R$ 499,99</strong></p>
-                    </div>
-                </div>
-
+                {slideAtual}
             </div>
 
         </section>
