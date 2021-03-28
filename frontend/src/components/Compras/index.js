4ffe72e9d833ce935e664735c57550e168ff384d
alineogassawara <linepsouza@gmail.com>
2021-03-28 11:50:35 -0300

    adicionando frete e cupom

--- frontend/src/components/Compras/index.js
+++ frontend/src/components/Compras/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
-import truck from "../../assets/img/carrinho/icon-truck.svg"
+import truck from "../../assets/img/carrinho/icon-truck.svg";
+import produto from "../../assets/img/carrinho/produto-carrinho.jpeg";
 
 const Compras = () => {
     return (
         <main class="page-row page-row-expanded">
-            <h3 class="mensagem">Este é o seu carrinho!</h3>
-            <div className="tela-carrinho">
+            <br />
+            <h4 class="titulo"> Este é o seu carrinho!</h4>
+            <div className="container-tela-carrinho">
                 <div className="lista-produtos">
                     <table class="table-sm">
                         <thead>
@@ -17,7 +19,7 @@ const Compras = () => {
                             </tr>
                         </thead>    
                         <tr>
-                            <td><img></img></td>
+                            <td><img class="produto-escolhido" src={produto} alt="produto" /></td>
                             <td>R$10,00</td>
                             <td>2</td>
                             <td>R$20,00</td>
@@ -25,34 +27,55 @@ const Compras = () => {
                     </table>
                 </div>
                 <hr />
-                <div className="resumo-carrinho">
-                    <table className="resumo" class="table-sm">
-                        <tr>
-                            <th>Sub Total:</th>
-                            <td>R$20,00</td>
-                        </tr>
-                        <tr>
-                            <th>Frete:</th>
-                            <td>(à calcular)</td>
-                        </tr>
-                        <tr>
-                            <th>Total à vista:</th>
-                            <td>R$20,00</td>
-                        </tr>
-                    </table>
-                </div>
+                <div>
+                    <h4 class="mensagem2">Resumo do Carrinho</h4>
+                    <div className="container-resumo-carrinho">
+                        <table className="resumo" class="table-sm">
+                            <tr>
+                                <th>Sub Total:</th>
+                                <td>R$20,00</td>
+                            </tr>
+                            <tr>
+                                <th>Frete:</th>
+                                <td>(à calcular)</td>
+                            </tr>
+                            <tr>
+                                <th>Total à vista:</th>
+                                <td>R$20,00</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>    
+                <hr />
                 <div class="calcule-frete">
                     <img class="truck-icon" src={truck} alt="frete"/>
                     <label>Calcule o frete</label>
                 </div>
                 <form>
-                   <div class="form-frete">
+                    <div class="form-frete">
                         <input type="text" class="form-input" placeholder="Digite o seu CEP"/>
-                   </div> 
-                   <div class="button">
+                    </div> 
+                    <div class="button">
                        <button type="submit" class="bnt-button">Calcular</button>
-                   </div>
+                    </div>  
                 </form>
+                <hr />
+                <form>
+                    <h5>Cupom de desconto</h5>
+                    <h7>Informe seu Cupom de Desconto</h7> 
+                    <div class="form-cupom">
+                        <input type="text" class="form-input" placeholder="Inserir código do cupom"/>
+                    </div> 
+                    <div class="button">
+                       <button type="submit" class="bnt-button">Ok</button>
+                    </div>
+                </form>
+                <hr />
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <button class="btn btn-primary btn-sm" type="button">Finalizar compra</button>
+                    <br />
+                    <button class="btn btn-outline-secondary btn-sm" type="button">Continuar comprando</button>
+                </div>
             </div>
         </main>    
     )
