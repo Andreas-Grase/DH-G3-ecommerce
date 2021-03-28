668df7142e7fec7b2421e15b129242aa43caba74
alineogassawara <linepsouza@gmail.com>
2021-03-28 17:34:08 -0300

    Melhorando página de carrinho

--- frontend/src/components/Compras/index.js
+++ frontend/src/components/Compras/index.js
@@ -4,12 +4,12 @@ import produto from "../../assets/img/carrinho/produto-carrinho.jpeg";
 
 const Compras = () => {
     return (
-        <main class="page-row page-row-expanded">
+        <main>
             <br />
             <h4 class="titulo"> Este é o seu carrinho!</h4>
-            <div className="container-tela-carrinho">
-                <div className="lista-produtos">
-                    <table class="table-sm">
+            <div class="flex-container">
+                <div class="flex-item">
+                    <table class="table align-middle">
                         <thead>
                             <tr class="titulo-lista">
                                 <th>Produto</th>
@@ -27,10 +27,10 @@ const Compras = () => {
                     </table>
                 </div>
                 <hr />
-                <div>
+                <div class="flex-item">
                     <h4 class="mensagem2">Resumo do Carrinho</h4>
                     <div className="container-resumo-carrinho">
-                        <table className="resumo" class="table-sm">
+                        <table className="resumo" class="table table-borderless">
                             <tr>
                                 <th>Sub Total:</th>
                                 <td>R$20,00</td>
@@ -45,42 +45,43 @@ const Compras = () => {
                             </tr>
                         </table>
                     </div>
-                </div>    
-                <hr />
-                <div class="calcule-frete">
-                    <img class="truck-icon" src={truck} alt="frete"/>
-                    <label>Calcule o frete</label>
-                </div>
-                <form>
-                    <div class="form-frete">
-                        <input type="text" class="form-input" placeholder="Digite o seu CEP"/>
-                    </div> 
-                    <div class="button">
-                       <button type="submit" class="bnt-button">Calcular</button>
-                    </div>  
-                </form>
-                <hr />
-                <form>
-                    <h5>Cupom de desconto</h5>
-                    <h7>Informe seu Cupom de Desconto</h7> 
-                    <div class="form-cupom">
-                        <input type="text" class="form-input" placeholder="Inserir código do cupom"/>
-                    </div> 
-                    <div class="button">
-                       <button type="submit" class="bnt-button">Ok</button>
+                    <hr />
+                    <div class="calcule-frete">
+                        <img class="truck-icon" src={truck} alt="frete"/>
+                        <label>Calcule o frete</label>
                     </div>
-                </form>
-                <hr />
-                <div class="d-grid gap-2 col-6 mx-auto">
-                    <button class="btn btn-primary btn-sm" type="button">Finalizar compra</button>
                     <br />
-                    <button class="btn btn-outline-secondary btn-sm" type="button">Continuar comprando</button>
-                </div>
+                    <form>
+                        <div class="form-frete">
+                            <input type="text" class="form-input" placeholder="Digite o seu CEP"/>
+                        </div> 
+                        <div class="button">
+                        <button type="submit" class="button">Calcular</button>
+                        </div>  
+                    </form>
+                    <hr />
+                    <form>
+                        <h4 class="mensagem2">Cupom de desconto</h4>
+                        <h5 class="subtitle">Informe seu Cupom de Desconto</h5> 
+                        <br />
+                        <div class="form-cupom">
+                            <input type="text" class="form-input" placeholder="Inserir código do cupom"/>
+                        </div> 
+                        <div class="button">
+                        <button type="submit" class="button">Ok</button>
+                        </div>
+                    </form>
+                    <hr />
+                    <div class="button-end">
+                        <button class="button-finalizar">Finalizar compra</button>
+                        <br />
+                        <button class="button-continuar">Continuar comprando</button>
+                    </div>
+                </div>    
             </div>
         </main>    
     )
 }
-
 
 export default Compras;
 
