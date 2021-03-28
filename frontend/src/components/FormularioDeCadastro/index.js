73a36332b94ff30cd3ab5634e9d42452758fb5ab
Andreas Grase <andreas.grase@gmail.com>
2021-03-28 19:22:00 -0300

    bugfix:consertando header e footer duplicados

--- frontend/src/components/FormularioDeCadastro/index.js
+++ frontend/src/components/FormularioDeCadastro/index.js
@@ -1,8 +1,16 @@
+import iconeCadastro from "../../assets/img/FormularioDeCadastro/iconeCadastro.svg"
 import "./styles.css"
 
 const FormularioDeCadastro = () => {
     return (
-        <p>teste</p>
+        <>
+            <div class="container-form-cadastro">
+                <div class="barra-superior">
+                    <img class="icone-de-cadastro" src={iconeCadastro} alt="icone de cadastro"/>
+                    <h2 class="titulo-do-cadastro">Cadastro</h2>
+                </div>
+            </div>
+        </>
     )
 }
 
--- frontend/src/pages/Cadastro/index.js
+++ frontend/src/pages/Cadastro/index.js
@@ -1,14 +1,10 @@
-import Header from "../../components/Header";
 import FormularioDeCadastro from "../../components/FormularioDeCadastro"
-import Footer from "../../components/Footer";
 import "./styles.css";
 
 const Cadastro = () => {
     return (
         <>
-            <Header />
             <FormularioDeCadastro />
-            <Footer />
         </>
     )
 }
