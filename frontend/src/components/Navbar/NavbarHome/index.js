953d1e1d3a7c6b6836b04c68ce8c5f838be6fa34
Daniel-de-Andrade <consultordanielazevedo@gmail.com>
2021-03-28 00:25:59 -0300

    feature[02]/resolvendo problemas do merge anterior

--- frontend/src/components/Navbar/NavbarHome/index.js
+++ frontend/src/components/Navbar/NavbarHome/index.js
@@ -18,10 +18,10 @@ const NavbarHome = () => {
           <NavLink exact to="/" activeStyle>Início</NavLink>
           <NavLink to="/sobre" activeStyle>Sobre</NavLink>
           <NavLink to="/contato" activeStyle>Contato</NavLink>
-          <NavLink to="/produtos" activeStyle>Produtos</NavLink>
           <NavLink to="/categorias" activeStyle>Categorias</NavLink>
           <NavLink to="/marcas" activeStyle>Marcas</NavLink>
-          <NavLink to="/promocoes" activeStyle>Promoções</NavLink>
+          <NavLink to="/produtos" activeStyle>Produtos</NavLink>
+          <NavLink to="/promocoes" activeStyle>Promoções</NavLink>          
           {/* 2nd Nav */}
           {/* <NavBtn>
           <NavBtnLink to="/cadastre-se">Cadastre-se</NavBtnLink>
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -4,20 +4,17 @@ import Marcas from "../../components/Marcas";
 import Categorias from "../../components/Categorias";
 import Ofertas from "../../components/Ofertas";
 import SliderHome from "../../components/Slider";
-import FinalizarCompras from "../FinalizarCompras"
+import FinalizarCompras from "../Carrinho/FinalizarCompras"
 import "./styles.css";
 
 
 const Home = () => {
   return (
     <>
-      <Header />
       <SliderHome />
       <Categorias />
       <Ofertas />
       <Marcas />
-      <Footer />
-      <FinalizarCompras />
     </>
   );
 };
