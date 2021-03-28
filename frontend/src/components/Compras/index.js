eaedebf0669f7e2572816135dbc5a8be3075ad36
alineogassawara <linepsouza@gmail.com>
2021-03-28 19:00:45 -0300

    ajustes página de carrinho

--- frontend/src/components/Compras/index.js
+++ frontend/src/components/Compras/index.js
@@ -28,55 +28,57 @@ const Compras = () => {
                 </div>
                 <hr />
                 <div class="flex-item">
-                    <h4 class="mensagem2">Resumo do Carrinho</h4>
-                    <div className="container-resumo-carrinho">
-                        <table className="resumo" class="table table-borderless">
-                            <tr>
-                                <th>Sub Total:</th>
-                                <td>R$20,00</td>
-                            </tr>
-                            <tr>
-                                <th>Frete:</th>
-                                <td>(à calcular)</td>
-                            </tr>
-                            <tr>
-                                <th>Total à vista:</th>
-                                <td>R$20,00</td>
-                            </tr>
-                        </table>
-                    </div>
-                    <hr />
-                    <div class="calcule-frete">
-                        <img class="truck-icon" src={truck} alt="frete"/>
-                        <label>Calcule o frete</label>
-                    </div>
-                    <br />
-                    <form>
-                        <div class="form-frete">
-                            <input type="text" class="form-input" placeholder="Digite o seu CEP"/>
-                        </div> 
-                        <div class="button">
-                        <button type="submit" class="button">Calcular</button>
-                        </div>  
-                    </form>
-                    <hr />
-                    <form>
-                        <h4 class="mensagem2">Cupom de desconto</h4>
-                        <h5 class="subtitle">Informe seu Cupom de Desconto</h5> 
-                        <br />
-                        <div class="form-cupom">
-                            <input type="text" class="form-input" placeholder="Inserir código do cupom"/>
-                        </div> 
-                        <div class="button">
-                        <button type="submit" class="button">Ok</button>
+                    <div class="container2">
+                        <h4 class="mensagem2">Resumo do Carrinho</h4>
+                        <div className="container-resumo-carrinho">
+                            <table className="resumo" class="table table-sm table-borderless">
+                                <tr>
+                                    <th>Sub Total:</th>
+                                    <td>R$20,00</td>
+                                </tr>
+                                <tr>
+                                    <th>Frete:</th>
+                                    <td>(à calcular)</td>
+                                </tr>
+                                <tr>
+                                    <th>Total à vista:</th>
+                                    <td>R$20,00</td>
+                                </tr>
+                            </table>
                         </div>
-                    </form>
-                    <hr />
-                    <div class="button-end">
-                        <button class="button-finalizar">Finalizar compra</button>
+                        <hr />
+                        <div class="calcule-frete">
+                            <img class="truck-icon" src={truck} alt="frete"/>
+                            <label>Calcule o frete</label>
+                        </div>
                         <br />
-                        <button class="button-continuar">Continuar comprando</button>
-                    </div>
+                        <form>
+                            <div class="form-frete">
+                                <input type="text" class="form-input" placeholder="Digite o seu CEP"/>
+                            </div> 
+                            <div class="button">
+                            <button type="submit" class="button">Calcular</button>
+                            </div>  
+                        </form>
+                        <hr />
+                        <form>
+                            <h4 class="mensagem2">Cupom de desconto</h4>
+                            <h5 class="subtitle">Informe seu Cupom de Desconto</h5> 
+                            <br />
+                            <div class="form-cupom">
+                                <input type="text" class="form-input" placeholder="Inserir código do cupom"/>
+                            </div> 
+                            <div class="button">
+                            <button type="submit" class="button">Ok</button>
+                            </div>
+                        </form>
+                        <hr />
+                        <div class="button-end">
+                            <button class="button-finalizar">Finalizar compra</button>
+                            <br />
+                            <button class="button-continuar">Continuar comprando</button>
+                        </div>
+                    </div>    
                 </div>    
             </div>
         </main>    
