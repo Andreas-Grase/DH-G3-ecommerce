a43aeaf03dfbb4d27586836ea1054a895acf8279
Andreas Grase <andreas.grase@gmail.com>
2021-04-01 01:28:45 -0300

    cadastro adaptado - corrigir

--- frontend/src/components/FormularioDeCadastro/index copy.js
+++ frontend/src/components/FormularioDeCadastro/index copy.js
@@ -5,13 +5,16 @@ import "./styles.css"
 const FormPasso1 = () => {
     return (
         <form className="form">
-            <h2 className="titulo-do-form-do-passo">Seu acesso</h2>            
-            <p>Coloque seu usuário:</p>
+            <h2 className="titulo-do-form-do-passo">Seu acesso</h2>
+            <p>Defina seus dados de acesso</p>
+            <p>Coloque seu nome de usuário:</p>
             <input type="text" name="" placeholder="Seu nome de usuario" />
             <p>Coloque seu email:</p>
             <input type="email" name="" placeholder="Seu email" />
             <p>Crie uma senha:</p>
-            <input type="password" name="" placeholde="Sua senha" />
+            <input type="password" />
+            <p>Repita a senha:</p>
+            <input type="password" />
         </form>
     )
 }
@@ -19,9 +22,10 @@ const FormPasso1 = () => {
 const FormPasso2 = () => {
     return (
         <form className="form">
-            <h2 className="titulo-do-form-do-passo">Seu endereço</h2>
+            <h2 className="titulo-do-form-do-passo">Coloque seu endereço abaixo</h2>
+            <p>Esse endereço é para o qual enviaremos seus produtos. Você também pode fazer envios para outros endereços.</p>
             <p>CEP</p>
-            <input type="text" name="" placeholder="Seu cep" />
+            <input type="number" name="" placeholder="Seu cep. O endereço será preenchido automaticamente." />
             <p>Rua</p>
             <input type="text" name="" placeholder="Sua rua" />
             <p>Complemento</p>
@@ -50,32 +54,31 @@ const FormularioDeCadastro = () => {
     const [formAtual, setPasso] = useState(FormPasso1)
 
     const proximo = () => {
-        setPasso(FormPasso2)
+            setPasso(FormPasso2)
     }
 
     return (
         <div class="container-externo">
             <div class="container-interno">
                 <img src={logo} className="icone-container-interno" width="100" height="100" alt="" />
-                <h1>Cadastro</h1>
+                <h1 className="titulo-cadastro">Cadastro</h1>
                 <div className="container-formulario">
-
-                    <div className="lista-de-passos">
-                        <div className="passo">
-                            <h3>Passo 1</h3>
-                            <p>Dados de acesso</p>
+                    <div className="coluna-esquerda">
+                        <div className="lista-de-passos">
+                            <div className="passo">
+                                <h3>Passo 1</h3>
+                                <p>Dados de acesso</p>
+                            </div>
+                            <div className="passo">
+                                <h3>Passo 2</h3>
+                                <p>Endereço de entrega</p>
+                            </div>
+                            <div className="passo">
+                                <h3>Passo 3</h3>
+                                <p>Email de confirmação</p>
+                            </div>
                         </div>
-                        <div className="passo">
-                            <h3>Passo 2</h3>
-                            <p>Endereço de entrega</p>
-                        </div>
-                        <div className="passo">
-                            <h3>Passo 3</h3>
-                            <p>Email de confirmação</p>
-                        </div>
-                        <div className="botao-proximo">
-                            <button onClick={proximo}>Próximo</button>
-                        </div>
+                        <button onClick={proximo}>Próximo</button>
                     </div>
                     <div className="formularioAtual">
                         {formAtual}
