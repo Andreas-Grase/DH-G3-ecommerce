8b3e51cf94d098f9345bc5d708e37d48843a2159
Daniel-de-Andrade <consultordanielazevedo@gmail.com>
2021-06-01 20:08:55 -0300

    removendo node_modules

--- frontend/src/components/modals/AtualizarProduto/index.js
+++ frontend/src/components/modals/AtualizarProduto/index.js
@@ -11,20 +11,27 @@ import {
   ModalFooter,
   ModalHeader,
 } from "reactstrap";
+import axios from "axios";
 import "./style.css";
 
 const ModalAtualizar = ({ product }) => {
+  const [id, setId] = useState(product.id);
   const [nome, setNome] = useState(product.nome);
   const [categoria, setCategoria] = useState(product.categoria);
   const [estoque, setEstoque] = useState(product.estoque);
   const [preco, setPreco] = useState(product.preco);
 
+  const handleUpdate = async () => {};
   return (
     <Modal id="myModal" isOpen={true}>
       <ModalHeader>{`Atualizar produto ${product.nome} de id ${product.id}`}</ModalHeader>
       <div className="modal-container">
         <div id="add-img">
-          <img className="image-upload" src={produtomarca1} alt="produto-atualizado"/>
+          <img
+            className="image-upload"
+            src={produtomarca1}
+            alt="produto-atualizado"
+          />
           <button id="upload-btn">Upload</button>
         </div>
         <div className="modal-body">
@@ -70,7 +77,7 @@ const ModalAtualizar = ({ product }) => {
             <Button
               id="buttonNAV"
               className="btn btn-primary"
-              href="/dashboard/produto/Atualizar/:id"
+              onClick={handleUpdate}
             >
               Atualizar
             </Button>
