ae34c7eca9bd3c7309b65f6645deb5b6c7a68f3c
tatasevilla <tatasevilla2@hotmail.com>
2021-06-01 14:07:58 -0300

    feature/adicionando estilo à nova lista de produtos

--- frontend/src/components/ProdutosDOS/Product.js
+++ frontend/src/components/ProdutosDOS/Product.js
@@ -8,11 +8,11 @@ const Product = () => {
                 {storeProducts.map((data, key) => {
                     return (
                         <div key={key} className="cardzinho">
-                            <h5>{data.title}</h5>
                             <a href=""><img src={data.img} alt="Detalhes do Produto" /></a>
+                            <h5>{data.title}</h5>
                             <h5>{data.company}</h5>
                             <h5>${data.price}</h5>
-                            <button id="boton-carrito" class="btn" href="#">Adicionar ao carrinho <i class="fas fa-cart-plus"></i></button>
+                            <button id="boton-carrito" className="btn" href="#">Adicionar ao carrinho <i className="fas fa-cart-plus"></i></button>
                         </div>
 
                     );
