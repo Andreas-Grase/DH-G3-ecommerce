6d47641fbb35a6d00f8d7036f6021991317323fd
Daniel-de-Andrade <consultordanielazevedo@gmail.com>
2021-06-02 15:22:45 -0300

    ajustando botões mobile e titulos cards categorias na page home

--- frontend/src/components/Categorias/index.js
+++ frontend/src/components/Categorias/index.js
@@ -9,24 +9,29 @@ const Categorias = () => {
   return (
     <section id="produtos">
       <div className="titulo-categorias">
-        <h2>Nossos Produtos</h2>
-        <h4>Conheça nossas categorias</h4>
+        <h1>Nossos Produtos</h1>
+        <h2>Conheça nossas categorias</h2>
       </div>
       <div id="ladoalado">
         <a href="/produtos">
           <img className="img-produtos1" src={barbearia} alt="shampoo" />
+          <h3>Barbearia</h3>
         </a>
         <a href="/produtos">
           <img className="img-produtos" src={cabelo} alt="condicionador" />
+          <h3>Cabelo</h3>
         </a>
         <a href="/produtos">
           <img className="img-produtos1" src={lancamentos} alt="mascaras" />
+          <h3>Lançamentos</h3>
         </a>
         <a href="/produtos">
           <img className="img-produtos" src={pele} alt="cremes" />
+          <h3>Pele</h3>
         </a>
         <a href="/produtos">
           <img className="img-produtos1" src={unhas} alt="barbearia" />
+          <h3>Unhas</h3>
         </a>
       </div>
     </section>
